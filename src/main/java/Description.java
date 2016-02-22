8f3169154d8e772eb5a79378a4da09585c96a879
Giraudux <giraudux@gmail.com>
2016-02-22 17:14:40 +0100

    complete description

--- src/main/java/Description.java
+++ src/main/java/Description.java
@@ -1,3 +1,67 @@
 
+/**
+ *
+ */
 public class Description {
+    private String name;
+    private String description;
+    private boolean singleton;
+    private boolean autorun;
+    private boolean proxy;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isSingleton() {
+        return singleton;
+    }
+
+    public void setSingleton(boolean singleton) {
+        this.singleton = singleton;
+    }
+
+    public boolean isAutorun() {
+        return autorun;
+    }
+
+    public void setAutorun(boolean autorun) {
+        this.autorun = autorun;
+    }
+
+    public boolean isProxy() {
+        return proxy;
+    }
+
+    public void setProxy(boolean proxy) {
+        this.proxy = proxy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Description that = (Description) o;
+
+        return name.equals(that.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
