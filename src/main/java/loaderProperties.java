a90e489c9fabddfc52ac784a82071b4487bfebe8
Nina EXPOSITO <E15A893B@I019V7pc05.ensinfo.sciences.univ-nantes.prive>
2016-03-07 17:41:56 +0100

    ajout des contributors

---
+++ src/main/java/loaderProperties.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class loaderProperties {
+	
+	public void startLoaderProperties(String fileProperties) throws IOException{
+
+	}
+}
